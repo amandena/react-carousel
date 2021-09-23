e27fc550c03ff9fb959b4f0096add69d23071270
amandena <manda.m.johnson@gmail.com>
2021-09-22 23:57:15 -0400

    imported carousel component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import TheOfficeCarousel from './components/TheOfficeCarousel'
 
 function App() {
   return (
     <div className='App'>
-      React Carousel
+      <TheOfficeCarousel/>
     </div>
   )
 }

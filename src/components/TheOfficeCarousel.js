0eac1c8dbade1a7eeea098a2d53e500863521d9c
amandena <manda.m.johnson@gmail.com>
2021-09-23 23:47:05 -0400

    manually started the carousel and added interval and wrap specifications

--- src/components/TheOfficeCarousel.js
+++ src/components/TheOfficeCarousel.js
@@ -1,11 +1,22 @@
+import { Carousel } from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import { useEffect } from 'react'
 import problems from '../assets/99problems.jpeg'
 import britney from '../assets/britney.jpeg'
 import nobodyAsked from '../assets/nobodyAsked.jpeg'
 
 const TheOfficeCarousel = () => {
+
+  useEffect(() => {
+    let myCarousel = document.querySelector('#theOfficeCarousel')
+    new Carousel(myCarousel, {
+      interval: 1000,
+      wrap: true
+    })
+  }, [])
+
   return (
     <div className='container'>
-      <div id="theOfficeCarousel" className="carousel slide" data-bs-ride="carousel">
+      <div id="theOfficeCarousel" className="carousel slide">
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img src={problems} className="d-block w-100" alt="99 problems"/>

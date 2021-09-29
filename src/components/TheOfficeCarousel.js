0d4670127b5e53d44a341fd7eb6cfdeb92b973c9
amandena <manda.m.johnson@gmail.com>
2021-09-29 19:31:58 -0400

    trying to simplify indicators

--- src/components/TheOfficeCarousel.js
+++ src/components/TheOfficeCarousel.js
@@ -42,11 +42,14 @@ const TheOfficeCarousel = () => {
           </i>
           <span class="visually-hidden">Next</span>
         </button>
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <ol class="carousel-indicators">
+          {/* <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
+          [...Array(3)].map((item, i) => {
+            return <li key={i} className={`bg-secondary ${i === 0 ? 'active' : ''}`} data-bs-target="#theOfficeCarousel" data-bs-slide-to={i} aria-current={`${i === 0 ? true : false}`} aria-label={`Slide ${i}`}></li>
+          })
+        </ol>
       </div>
     </div>
   )

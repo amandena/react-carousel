fac18c8d7bf65bd4cb913518229c94e826e66253
amandena <manda.m.johnson@gmail.com>
2021-09-28 21:52:02 -0400

    added bs indicators

--- src/components/TheOfficeCarousel.js
+++ src/components/TheOfficeCarousel.js
@@ -42,6 +42,11 @@ const TheOfficeCarousel = () => {
           </i>
           <span class="visually-hidden">Next</span>
         </button>
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#theOfficeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
       </div>
     </div>
   )
